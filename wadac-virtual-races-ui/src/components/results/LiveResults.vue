f2b7e69ea2dacebcec4e5808e51878644e0758f6
Dan Cunnington <DanCunnington@uk.ibm.com>
2020-12-21 14:37:50 +0000

    followed set course and duathlon results live results page

--- wadac-virtual-races-ui/src/components/results/LiveResults.vue
+++ wadac-virtual-races-ui/src/components/results/LiveResults.vue
@@ -15,11 +15,11 @@
       <p>Loading...</p>
     </div>
     <div v-if="!wcr && preview_results_set.length > 0">
-      <div class="event-stats">
+      <div class="event-stats" v-if="!duathlon">
         <p><span class="stat-label">Event Distance:</span> <span class="ref-distance">{{ref_distance}}</span> mi</p>
         <p><span class="stat-label">Event Elevation Gain:</span> <span class="ref-distance">{{ref_elevation_gain}}</span> ft</p>
       </div>
-      <div class="event-stats">
+      <div class="event-stats" v-if="!duathlon">
         <p class="net-chng" v-if="ref_elevation_change"><span class="stat-label">Event Net Elevation Change:</span> <span class="ref-distance">{{ref_elevation_change}}</span> ft</p>
       </div>
       <b-table striped hover sticky-header 
@@ -65,6 +65,7 @@ export default {
       ref_elevation_gain: false,
       ref_elevation_change: false,
       wcr: false,
+      duathlon: false,
       show_results: false
     }
   },
@@ -127,6 +128,7 @@ export default {
         } else {
           this.preview_results_set = response.data
           this.wcr = item.wcr_event
+          this.duathlon = item.duathlon_event
           this.ref_distance = item.distance
           this.ref_elevation_gain = item.elevation_gain
           this.ref_elevation_change = item.elevation_change
@@ -135,19 +137,55 @@ export default {
             {"key": "pos", "sortable": false},
             {"key": "athlete_name", "sortable": false}
           ]
-          if (this.ref_distance && this.ref_elevation_gain && this.ref_elevation_change) {
+          if (this.duathlon) {
+            console.log('duathlon')
+            this.fields.push({"key": "adjusted_time", "label": "Adjusted Time (s)", "sortable": false, "thClass": 'd-none', "tdClass": 'd-none'})
+            this.fields.push({"key": "hms", "label": "Adjusted Time", "sortable": false})
+            this.preview_results_set.forEach((r, idx) => {
+              r.adjusted_time = r['moving_time']
+              let h = parseInt(r.adjusted_time / 3600).toString()
+              let m = parseInt((r.adjusted_time % 3600)/60).toString()
+              let s = parseInt((r.adjusted_time % 3600)%60).toString()
+              if (h.length == 1) {
+                  h = '0'+h
+              }
+              if (m.length == 1) {
+                  m = '0'+m
+              }
+              if (s.length == 1) {
+                  s = '0'+s
+              }
+              let hms_str = h+':'+m+':'+s
+              r.hms = hms_str
+              
+            })
+          } else if (this.ref_distance && this.ref_elevation_gain && this.ref_elevation_change) {
             this.fields.push({"key": "adjusted_time", "label": "Adjusted Time (s)", "sortable": false, "thClass": 'd-none', "tdClass": 'd-none'})
             this.fields.push({"key": "hms", "label": "Adjusted Time", "sortable": false})
 
             // Adjust time to match event
             this.preview_results_set.forEach((r, idx) => {
-              let adj_obj = API.calculateAdjustedTime(parseFloat(this.ref_distance), parseFloat(this.ref_elevation_gain), 
+              let followed_set_course = r['followed_set_course']
+              if (!followed_set_course || followed_set_course == undefined) {
+                followed_set_course = "no"
+              }
+
+              let adj_obj = API.calculateAdjustedTime(parseFloat(this.ref_distance), parseFloat(this.ref_elevation_gain),
                 parseFloat(this.ref_elevation_change), parseFloat(r['distance']), parseFloat(r['moving_time']), 
-                parseFloat(r['elevation_gain']), parseFloat(r['net_elevation_change']))
+                parseFloat(r['elevation_gain']), parseFloat(r['net_elevation_change']), followed_set_course)
+              let adj_time = adj_obj['adj_time']
+              let ref_dist = adj_obj['ref_distance']
+              let ref_elev = adj_obj['ref_elevation_gain']
+              let ref_elev_change = adj_obj['ref_elevation_change']
               let hms = adj_obj['hms_str']
+
+              r.ref_distance = ref_dist
+              r.ref_elevation_gain = ref_elev
+              r.ref_elevation_change = ref_elev_change
+              r.adjusted_time = adj_time
               r.hms = hms
-              r.adjusted_time = adj_obj['adj_time']
               r.pos = idx + 1
+
             })
           } else if (this.ref_distance && this.ref_elevation_gain) {
             this.fields.push({"key": "adjusted_time", "label": "Adjusted Time (s)", "sortable": false, "thClass": 'd-none', "tdClass": 'd-none'})
